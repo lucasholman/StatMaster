395ddf56ae40bf73cd734b4d7c65b2ddb0bc27dc
liamho <liamviippola@gmail.com>
2017-02-10 01:20:51 -0500

    Added items to DB

--- Project/StatMaster/StatMaster/Form1.Designer.cs
+++ Project/StatMaster/StatMaster/Form1.Designer.cs
@@ -29,11 +29,45 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.statMasterDBDataSet = new StatMaster.StatMasterDBDataSet();
+            this.itemsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.itemsTableAdapter = new StatMaster.StatMasterDBDataSetTableAdapters.ItemsTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.statMasterDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statMasterDBDataSet
+            // 
+            this.statMasterDBDataSet.DataSetName = "StatMasterDBDataSet";
+            this.statMasterDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // itemsBindingSource
+            // 
+            this.itemsBindingSource.DataMember = "Items";
+            this.itemsBindingSource.DataSource = this.statMasterDBDataSet;
+            // 
+            // itemsTableAdapter
+            // 
+            this.itemsTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 321);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.statMasterDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private StatMasterDBDataSet statMasterDBDataSet;
+        private System.Windows.Forms.BindingSource itemsBindingSource;
+        private StatMasterDBDataSetTableAdapters.ItemsTableAdapter itemsTableAdapter;
     }
 }
 
